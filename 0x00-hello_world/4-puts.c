f4306cf90825082bdd54a07a8ce59a41fbdf2ea7
aijeuzor <adchekol@gmail.com>
2023-04-21 14:56:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -z $CFILE -o c
+include <stdio.h>
+**
+* main - Entry point
+* * Return: Always 0 (Success)
+* */
+int main (void)
+(
+ puts ("\" Programming is like building a multilingual puzzle")
+ return (0) ;
+ )

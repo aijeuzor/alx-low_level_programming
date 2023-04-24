1d4909f7e56e94862fb13e3045c4a28b700fe670
aijeuzor <adchekol@gmail.com>
2023-04-24 08:31:44 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - 3-print_alphabets.c
+*
+* Return: (0)
+*/
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+
+putchar('\n');
+
+return (0);
+}

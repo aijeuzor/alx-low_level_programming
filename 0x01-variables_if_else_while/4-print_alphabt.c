c915237b2b9c6d755b52eb19a9c838cf43944bc9
aijeuzor <adchekol@gmail.com>
2023-04-24 09:14:32 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - 4-print_alphabt.c
+*
+* Return: (0)
+*/
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'e' && letter != 'q');
+putchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+}

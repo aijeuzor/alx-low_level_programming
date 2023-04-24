4b62c0a98c20d0b215453e7abff8dbba9087fb56
aijeuzor <adchekol@gmail.com>
2023-04-24 14:39:06 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,8 @@ int main(void)
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if(letter != 'e' && letter != 'q');
-putchar(letter);
+	if (letter != 'e' && letter != 'q');
+	putchar(letter);
 }
 putchar('\n');
 return (0);

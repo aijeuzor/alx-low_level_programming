2cbfa152ed71d67411373cbcf7407e5b1613f089
aijeuzor <adchekol@gmail.com>
2023-04-30 12:39:43 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,22 +12,23 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3 == 0) && (i % 5 == 0))
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf("FizzBuzz\n");
-} 
-else if (i % 3 == 0)
+printf(" Fizz");
+} else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Fizz\n");
-}
-else if (i % 5 == 0)
+printf(" Buzz");
+} else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("Buzz\n");
-}
-else
+printf(" FizzBuzz");
+} else if (i == 1)
 {
-printf("%d\n", i);
+printf("%d", i);
+} else
+{
+printf(" %d", i);
 }
 }
+printf("\n");
 return (0);
 }

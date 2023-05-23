340086aaa60e1983128a20b5d1262ca2730d1308
aijeuzor <adchekol@gmail.com>
2023-05-23 05:36:11 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,23 +3,4 @@
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
-/**
-* main - computes the absolute value of a number
-*
-* Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	int num = -10;
-
-	printf("Absolute value of %d is %d\n", num, ABS(num));
-
-	double value = -3.14;
-
-	printf("Absolute value of %f is %f\n", value, ABS(value));
-
-	return (0);
-}
-
 #endif
